2d7d8115e906c379152ad4f880de73b59fcde3c5
deepak-420 <83333080+deepak-420@users.noreply.github.com>
2023-03-13 10:54:50 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require("dotenv").config()
 const connectToMongo=require('./db')
 const express = require('express')
 var cors = require('cors')
 connectToMongo()
 
 const app = express()
-const port = 5000
+const port = process.env.PORT 
 
 app.use(cors())
 app.use(express.json())
